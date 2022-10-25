fe60b92d287d72c0d290adc6a4b182961f3c98bb
jsyoo <eatdesignlove@gmail.com>
2022-10-25 12:56:48 +0900

    feat: Apply react-app-rewired

--- config-overrides.js
+++ config-overrides.js
@@ -1,4 +1,8 @@
-export default function override(config, env) {
-	//do stuff with the webpack config...
+// eslint-disable-next-line @typescript-eslint/no-var-requires, no-undef
+const { VanillaExtractPlugin } = require('@vanilla-extract/webpack-plugin');
+
+// eslint-disable-next-line no-undef
+module.exports = function override(config, env) {
+	config.plugins = [new VanillaExtractPlugin()];
 	return config;
-}
+};
